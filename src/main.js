2ad100a7e9274da36d6562b1d954d4276039d2b9
vsalfafara <alfafara.vm@gmail.com>
2022-03-22 21:27:38 +0800

    updated main js

--- src/main.js
+++ src/main.js
@@ -7,13 +7,10 @@ import './css/main.scss'
 import 'element-theme-dark'
 import VueSocketIOExt from 'vue-socket.io-extended'
 import { io } from 'socket.io-client'
-import VueClipboard from 'vue-clipboard2'
 
 const socket = io('http://103.253.146.189:5000/')
 // const socket = io('http://localhost:5000/')
 
-VueClipboard.config.autoSetContainer = true
-Vue.use(VueClipboard)
 Vue.config.productionTip = false
 Vue.use(VueSocketIOExt, socket)
 Vue.use(ElementUI)
